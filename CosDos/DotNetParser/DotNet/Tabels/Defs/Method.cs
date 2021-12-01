f6d146a8e375b65299a45d09aeb23c4591898ca9
eli310 <Elihillman@hotmail.com>
2021-12-02 01:49:01 +1100

    no

--- CosDos/DotNetParser/DotNet/Tabels/Defs/Method.cs
+++ CosDos/DotNetParser/DotNet/Tabels/Defs/Method.cs
@@ -1,6 +1,7 @@
 ﻿using LibDotNetParser.PE;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace LibDotNetParser.DotNet.Tabels.Defs
@@ -13,14 +14,9 @@ namespace LibDotNetParser.DotNet.Tabels.Defs
         public uint Name { get; private set; }
         public uint Signature { get; private set; }
         public uint ParamList { get; private set; }
+        [MethodImpl(MethodImplOptions.InternalCall)]
         public void Read(MetadataReader reader)
         {
-            RVA = reader.ReadUInt32();
-            ImplFlags = reader.ReadUInt16();
-            Flags = reader.ReadUInt16();
-            Name = reader.ReadStringStreamIndex();
-            Signature = reader.ReadBlobStreamIndex();
-            ParamList = reader.ReadUInt16();
         }
     }
 }
